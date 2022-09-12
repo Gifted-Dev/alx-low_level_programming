de899592bddd41f6c9120e07d4c660182b4e3b10
Gifted-Dev <starstrings02@gmail.com>
2022-09-12 15:23:51 -0700

    hot fix

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,17 +1,22 @@
-#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 /**
- * main - prints the alphabets in lower case
- * followed by a new line
- * Return: Always 0 (Success)
+ * main - Entry point
+ * Return: always (0) Success
  */
 int main(void)
 {
-	char ch;
+	int n;
 
-	for (ch = 'a'; ch <= 'z'; ch++)
-	{
-		putchar(ch);
-	}
-	putchar('\n')
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("Last digit of %d is %d ", n, n % 10);
+	if (n % 10 > 5)
+		printf("and is greater than 5\n");
+	else if (n % 10 == 0)
+		printf("and is 0\n");
+	else if (n % 10 < 6 && n % 10 != 0)
+		printf("and is less than 6 and not 0\n");
 	return (0);
 }
