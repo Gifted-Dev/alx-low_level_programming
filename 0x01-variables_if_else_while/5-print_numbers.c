d92962b8cff89ba094d9b32f80928a469d0228c8
Gifted-Dev <starstrings02@gmail.com>
2022-09-12 19:59:57 -0700

    New Task added

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - program to print digits of base 10 from zero
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+}
