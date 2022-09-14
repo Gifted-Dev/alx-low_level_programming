b327e33345eaebbe8ec69f19d8769b9bd92fbb76
Gifted-Dev <starstrings02@gmail.com>
2022-09-14 01:17:45 -0700

    New task added

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function to print alphabet
+ * 10 times
+ * Return: Always Success
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char chr;
+
+	while (i < 10)
+	{
+		chr = 'a';
+		while (chr <= 'z')
+		{
+			_putchar(chr);
+			chr++;
+		}
+
+		i++;
+	}
+}
